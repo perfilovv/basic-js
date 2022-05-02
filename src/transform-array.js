4198f1f6d2a1d9cf2b833f589d8e98188eee9c8f
Vadim <russianspiritoneshotlol@gmail.com>
2022-05-02 16:40:35 +0300

    transform-array task complite

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let newArr = [];
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-prev') {
+      arr[i - 1] && newArr.pop();
+    } else if (arr[i] === '--discard-next') {
+      arr[i + 1] && i++ && newArr.push('remove');
+    } else if (arr[i] === '--double-prev') {
+      arr[i - 1] && newArr.push(newArr[newArr.length - 1]);
+    } else if (arr[i] === '--double-next') {
+      arr[i + 1] && newArr.push(arr[i + 1]);
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr.filter((obj) => obj !== 'remove');
 }
 
 module.exports = {
